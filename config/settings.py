91c4770bed6909cdd70b061f64c7c805362fbb55
DaryaRu <89119484+DaryaRu@users.noreply.github.com>
2021-09-29 14:25:22 +0300

    jwt auth

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,3 +95,43 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+JWT_AUTH = {
+
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3000),
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_response_payload_handler',
+    
+    'JWT_ALLOW_REFRESH': False,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_AUTH_COOKIE': None,
+}
--- shop/viewsets/user_view.py
+++ shop/viewsets/user_view.py
@@ -2,8 +2,45 @@ from shop.serializers.user_serializer import UserSerializer
 from  rest_framework import generics
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
+from rest_framework_jwt.settings import api_settings
+from config.settings import SECRET_KEY
+from django.contrib.auth.signals import user_logged_in
+from rest_framework import status
+from rest_framework.response import Response
+
 
 class UserCreate(generics.CreateAPIView):
     queryset = get_user_model().objects
     serializer_class = UserSerializer
     permission_classes = (AllowAny, )
+    
+    def authenticate_user(request):
+
+        try:
+            username = request.data['username']
+            password = request.data['password']
+
+            user = get_user_model.objects.get(username=username, password=password)
+            if user:
+                try:
+                    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+                    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+                    payload = jwt_payload_handler(user)
+                    token = jwt_encode_handler(payload, SECRET_KEY)
+                    user_details = {}
+                    # user_details['name'] = "%s %s" % (
+                        # user.first_name, user.last_name)
+                    user_details['token'] = token
+                    user_logged_in.send(sender=user.__class__,
+                                    request=request, user=user)
+                    return Response(user_details, status=status.HTTP_200_OK)
+
+                except Exception as e:
+                    raise e
+            else:
+                res = {
+                    'error': 'can not authenticate with the given credentials or the account has been deactivated'}
+                return Response(res, status=status.HTTP_403_FORBIDDEN)
+        except KeyError:
+            res = {'error': 'please provide a username and a password'}
+            return Response(res)    
